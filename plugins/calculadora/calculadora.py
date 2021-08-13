4e0e816d0ebf7572bcaafd9bf33efa0dad68e631
Bruno-O20 <bruno.o20@aluno.ifsc.edu.br>
2021-08-13 01:15:21 +0000

    Criando comandos

--- plugins/calculadora/calculadora.py
+++ plugins/calculadora/calculadora.py
@@ -1,6 +1,30 @@
 from errbot import BotPlugin, botcmd
+from pyfiglet import Figlet
+# https://www.w3schools.com/python/ref_math_pi.asp
+from math import pi
 
-class Raio(BotPlugin):
+
+class Calculadora(BotPlugin):
+    """
+    Calculadora de operações matemáticas.
+    """
+    @botcmd
+    def comandos(self, msg, args):
+        """
+        Comandos do Bot.
+        """
+        yield "Para calcular a área de uma circunferência, utilize o comando !raio <valor em metros>"
+
     @botcmd(split_args_with=' ')
-    def settings():
-        return: Opções
+    def raio(self, msg, args):
+        """
+        Calcula o raio de um círculo.
+        """
+
+        try:
+            texto = args[0].replace (",", ".")
+            numero = float(texto)
+        except:
+            return "Não é um número real"
+        else:
+            return "```\n" + Figlet(font='slant').renderText(str(round(numero * numero * pi, 2))) + "\n```"
